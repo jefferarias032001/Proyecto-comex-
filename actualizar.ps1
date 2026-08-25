58af3fcddd19798801b7796a06ec18d76a1c996a
jefferarias032001 <jarias@tractocar.com>
2026-08-25 14:31:37 -0500

    Actualizar script: git add . para subir todo

--- actualizar.ps1
+++ actualizar.ps1
@@ -1,5 +1,5 @@
 cd "C:\Users\jarias\Desktop\tractocar-ventas"
 python procesar.py
-git add plantilla.html procesar.py .gitignore
+git add .
 git commit -m "Actualizacion automatica $(Get-Date -Format 'yyyy-MM-dd HH:mm')"
 git push origin main
